119f3c88c3107265c2716e7ec9582b7332d486d9
Uwe Lindner <bjoernuwe.linder@stud.uni-goettingen.de>
2023-06-09 15:46:46 +0200

    mergesort.py verändert

--- mergesort.py
+++ mergesort.py
@@ -51,7 +51,7 @@ my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+my_list = mergeSort(my_list)
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
