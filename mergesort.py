9e04012c8454f7ae253097fa23d59e811a1c9428
Uwe Lindner <bjoernuwe.linder@stud.uni-goettingen.de>
2023-06-09 16:43:55 +0200

    mergesort.py Plotting-Teil verbessert

--- mergesort.py
+++ mergesort.py
@@ -39,13 +39,27 @@ def mergeSort(list_to_sort_by_merge):
             i += 1
 
 
+#neuer Plotting-Teil
+
 import matplotlib.pyplot as plt
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
+ax1.plot(x, my_list)
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax1.set_title('Vor dem Sortieren')
+
+sorted_list = mergeSort(my_list)
+
 x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+ax2.plot(x, sorted_list)
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.set_title('Nach dem Sortieren')
+
+plt.subplots_adjust(wspace=0.3)
+
+plt.show
